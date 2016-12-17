3679217d37c85552079980fe7eafcbe4ef87f709
Ebrahim <ebrahim.el.gaml@gmail.com>
2016-12-17 16:14:45 +0200

    add alar sound

---
+++ Assets/AddedScripts/SceneFadeInOut.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour {
+	public float fadeSpeed = 1.5f;
+	private bool sceneStarting = true;
+
+	void Awake () {
+		GetComponent<GUITexture> ().pixelInset = new Rect (0f, 0f, Screen.width, Screen.height); 
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sceneStarting) {
+			StartScene ();
+		}
+	}
+
+	void FadeToClear ()
+	{
+		GetComponent<GUITexture> ().color = Color.Lerp (GetComponent<GUITexture> ().color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeToBlack ()
+	{
+		GetComponent<GUITexture> ().color = Color.Lerp (GetComponent<GUITexture> ().color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+	void StartScene() {
+		FadeToClear ();
+		if (GetComponent<GUITexture> ().color.a <= 0.05) {
+			float g = GetComponent<GUITexture> ().color.g;
+			float b = GetComponent<GUITexture> ().color.b;
+			GetComponent<GUITexture> ().color = new Color (Color.clear.r, g, b);
+			GetComponent<GUITexture> ().enabled = false;
+			sceneStarting = false;
+		}
+	}
+
+	public void EndScene() {
+		GetComponent<GUITexture> ().enabled = true;
+		FadeToBlack ();
+		if (GetComponent<GUITexture> ().color.a >= 0.95f) {
+			Application.LoadLevel (1);
+		}
+	}
+}
---
+++ Assets/AddedScripts/Tags.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+
+	public const string player = "Player";
+	public const string alarm = "AlarmLight";
+	public const string siren = "Siren";
+	public const string mainLight = "MainLight";
+	public const string gameController = "GameController";
+	public const string fader = "Fader";
+	public const string enemy = "Enemy";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
